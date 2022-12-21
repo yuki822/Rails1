b65423170955bee6c211b2bc8df475b892669da5
EC2 Default User <ec2-user@ip-172-31-13-242.ap-northeast-1.compute.internal>
2022-12-21 14:27:38 +0000

    rails課題1-間違えてpushした分の消去-

--- config/initializers/time_formats.rb
+++
@@ -1,2 +0,0 @@
-Date::DATE_FORMATS[:datetime_jp] = "%Y年 %m月%d日"
-Date::DATE_FORMATS[:datetime_jp_detail] = "%Y年 %m月 %d日 %H時 %M分"
--- db/migrate/20221213141828_add_columns_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :start_at, :date
-    add_column :posts, :end_at, :date
-    add_column :posts, :is_all_day, :date
-    add_column :posts, :memo, :string
-    add_column :posts, :not_before_today, :string
-  end
-end
