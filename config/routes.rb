7a02729681db7b1fdf79a8eb35dc02bb4b456870
EC2 Default User <ec2-user@ip-172-31-13-242.ap-northeast-1.compute.internal>
2023-01-05 16:01:41 +0000

    リポジトリ内のファイル削除

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'posts#index'
-  resources :posts
-
-  #get 'posts/index', to: 'posts#index'
-  #get 'lessons/hello', to: 'lessons#hello'
-  #get 'lessons/call', to: 'lessons#call'
-end
